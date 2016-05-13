24683eae7059b8af4da848e9294965f8cf04cf6c
cancer <zodiac.cancer.j6@gmail.com>
2016-05-14 08:03:09 +0900

    Duties should return if not has prices

--- server/app/controller/api/duties.js
+++ server/app/controller/api/duties.js
@@ -3,7 +3,7 @@ const app = require('../../index').app;
 const Duty = require('../../model/db/duty');
 
 app.get('/api/duties/', (req, res) => {
-  Duty.findHasPrices().exec((err, docs) => {
+  Duty.find().exec((err, docs) => {
     res.json(docs);
   });
 });
