0a9d6d7d0917db8def724bf4414601510b4f0da8
cancer <zodiac.cancer.j6@gmail.com>
2016-06-21 01:17:29 +0900

    add header Access-Control-Allow-Origin

--- server/app/controller/index.js
+++ server/app/controller/index.js
@@ -1,8 +1,10 @@
 'use strict';
-const app = require('express')();
+const app = require('../index').app;
+const corser = require('corser');
 
 module.exports = {
   run: () => {
+    app.use(corser.create());
     require('./api/duties');
     require('./api/prices');
   }
