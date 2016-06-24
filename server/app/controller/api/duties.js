aab1a1ea185e3c770f0f1d069eca397fdcb53243
cancer <zodiac.cancer.j6@gmail.com>
2016-06-25 07:58:16 +0900

    Fix API returns only have prices.

--- server/app/controller/api/duties.js
+++ server/app/controller/api/duties.js
@@ -3,7 +3,7 @@ const app = require('../../index').app;
 const Duty = require('../../model/db/duty');
 
 app.get('/api/duties/', (req, res) => {
-  Duty.find().exec((err, docs) => {
+  Duty.findHasPrices().exec((err, docs) => {
     res.json(docs);
   });
 });
